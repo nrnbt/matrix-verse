1aa438eedc30092327cdebc52989bb77f3504041
nrnbt <nrnbt0616@gmail.com>
2023-03-22 20:09:10 +0800

    mobile text resize

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,8 @@ const HomePage: FunctionComponent = () => {
       </Head>
       <div className='flex w-full h-screen pb-20 justify-center'>
         <div className='flex flex-col justify-center items-center text-center h-full w-2/5'>
-          <div className='text-4xl mb-8 px-4 py-2 neon-text rounded-xl'>Matrix-Verse</div>
-          <div className='text-xl font-bold'>GPT-3.5 models can understand and generate natural language or code. Our most capable and cost effective model in the GPT-3.5 family is gpt-3.5-turbo which has been optimized for chat but works well for traditional completions tasks as well.</div>
+          <div className='mb-8 px-4 py-2 neon-text rounded-xl text-md lg:text-4xl'>Matrix-Verse</div>
+          <div className='text-sm lg:text-xl font-bold'>GPT-3.5 models can understand and generate natural language or code. Our most capable and cost effective model in the GPT-3.5 family is gpt-3.5-turbo which has been optimized for chat but works well for traditional completions tasks as well.</div>
           <div className='flex flex-col justify-center items-center p-4 gap-4'>
             <div className='pt-4 font-semibold'>Choose one of below</div>
             <Navigations className='flex gap-4' />
